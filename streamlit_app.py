03849993cbad35574eebc4c62b0fcf70c9c404f8
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-03-28 16:47:46 +0200

    Update streamlit_app.py
    added kiwi to GET request for test

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # the next line normalizes the json response of fruityvice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
