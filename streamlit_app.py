40447648af6249ae89f32dadc28370f2302a4901
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-03-28 16:43:13 +0200

    Update streamlit_app.py
    normalizes json + converts to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,12 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just writes data to screen
+
+# the next line normalizes the json response of fruityvice
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# convert to streamlit dataframe
+streamlit.dataframe(fruityvice_normalized)
 
 
 
