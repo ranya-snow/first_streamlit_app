73f6e949411d442d9d8c357000d0e049a375c79c
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-03-28 17:21:01 +0200

    Update streamlit_app.py
    added connection parameters, querying metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import snowflake.connector
+
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -33,5 +33,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # convert to streamlit dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
