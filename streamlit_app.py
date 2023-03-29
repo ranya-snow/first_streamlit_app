51aeecede8fe27b9dc8ed0a8a3068ef652ef7e84
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-03-29 12:13:30 +0200

    Update streamlit_app.py
    close connection, specify variable to add to the list, close connections after button clicks, relabel header and buttons.

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,13 @@ def get_fruit_load_list():
     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     return my_cur.fetchall()
   
+streamlit.header("View our fruit list - Add your favorites!")
+  
 #Add a button to load the fruit:
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 #streamlit.stop()
@@ -60,11 +63,12 @@ if streamlit.button('Get Fruit Load List'):
 #Allow the user to add afruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load list values ('from streamlit')")
+    my_cur.execute("insert into fruit_load list values ('" + new_fruit +"')")
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
