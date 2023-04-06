d507c8491c40a63d7d0628af2585da25120e6d91
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-06 13:27:11 +0200

    Update clash_royale_app.py

--- clash_royale_app.py
+++ clash_royale_app.py
@@ -12,25 +12,32 @@ st.header('👎 Your Clash Losses 👎')
 
 st.header('🏆 Total Trophies Since April 1st, 2023 🏆')
 
-@st.cache_resource
-def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
-conn = init_connection()
+# @st.cache_resource
+# def init_connection():
+#     return snowflake.connector.connect(
+#         **st.secrets["snowflake"], client_session_keep_alive=True
+#     )
 
-st.stop()
+# conn = init_connection()
 
-def get_battlelog():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("SELECT * FROM BATTLELOG_FLAT")
-    return my_cur.fetchall()
+# st.stop()
+
+# def get_battlelog():
+#   with my_cnx.cursor() as my_cur:
+#     my_cur.execute("SELECT * FROM BATTLELOG_FLAT")
+#     return my_cur.fetchall()
   
-if st.button('Get Battlelog List'):
-  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-  my_data_rows = get_battlelog()
-  my_cnx.close()
-  st.dataframe(my_data_rows)
+# if st.button('Get Battlelog List'):
+#   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+#   my_data_rows = get_battlelog()
+#   my_cnx.close()
+#   st.dataframe(my_data_rows)
 
 
