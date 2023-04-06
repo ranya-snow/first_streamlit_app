8e2f9b7737f586353e3081dc563bf72c74d4dbfe
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-06 12:45:47 +0200

    Update clash_royale_app.py
    added emojis, headers, and snowflake function

--- clash_royale_app.py
+++ clash_royale_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas
+import requests
+import snowflake.connector
+
 
 st.title('Welcome to your Clash Royale Stats')
-st.header('Your Clash Wins')
-st.header('Your Clash Losses')
+
+st.header('👍 Your Clash Wins 👍')
+
+st.header('👎 Your Clash Losses 👎')
+
+st.header('🏆 Total Trophies Since April 1st, 2023 🏆')
+
+def get_battlelog():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM BATTLELOG_FLAT")
+    return my_cur.fetchall()
+
+
