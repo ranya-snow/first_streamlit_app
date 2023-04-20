9e4ea9111fc667df62aae5091ef18d92b3a9eb86
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-20 15:01:57 +0200

    Update streamlit_docs.py

--- streamlit_docs.py
+++ streamlit_docs.py
@@ -1,20 +1,6 @@
 # streamlit_app.py
+import streamlit as st
+st.header("I'm doing this.")
 
-import streamlit
-import snowflake.connector
-
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-# @st.cache_resource
-# def init_connection():
-#     return snowflake.connector.connect(
-#         **st.secrets["snowflake"], client_session_keep_alive=True
-#     )
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
