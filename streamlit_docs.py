789cf8649e2ef7249f8bdc175671f0d2f6f790bc
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-20 15:23:33 +0200

    Update streamlit_docs.py

--- streamlit_docs.py
+++ streamlit_docs.py
@@ -1,6 +1,6 @@
 # streamlit_app.py
 import streamlit as st
-st.header("I'm doing this.")
+st.title("I'm doing this.")
 
 age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
