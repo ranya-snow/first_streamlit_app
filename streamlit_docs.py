7b90ddf29ba0062168fd64e5b8fbe0e8ece6cab6
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-20 15:25:49 +0200

    Update streamlit_docs.py

--- streamlit_docs.py
+++ streamlit_docs.py
@@ -6,4 +6,4 @@ age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
 
 st_name = st.text_input('What i your name?', 'Ranya')
-st.write(f"Hell {st_name}"!)
+st.write(f"Hello {st_name}!")
