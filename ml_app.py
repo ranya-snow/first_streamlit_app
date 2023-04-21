6d642900425313cd845745480a668ea3bf502a39
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-21 10:47:46 +0200

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,4 +1,18 @@
 #ml_app
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
-st.write("Hello World!")
+#Page configuration
+st.set_page_config(
+  page_title = 'Simple Prediction App',
+  layout = 'wide',
+  initial_sidebar_state = 'expanded')
+
+#Title of the app
+st.title('Simple Prediction App')
+
+#Load dataset
+df = pd.read_csv('https://raw.github.com/dataprofessor/data.master/iris.csv')
+st.write(df)
