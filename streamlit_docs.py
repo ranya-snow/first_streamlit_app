2722daac59a1d815d39462db306c93fb812dfdc9
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-04-25 14:25:16 +0200

    Update streamlit_docs.py

--- streamlit_docs.py
+++ streamlit_docs.py
@@ -32,6 +32,7 @@ animal_data = {
     "Class": ["Mammal", "Mammal", "Fish", "Reptile", "Bird", "Bird", "Mammal", "Reptile", "Fish", "Bird", "Mammal", "Reptile", "Fish", "Reptile", "Bird"]
 }
 
+#Convert the dictionary to a Pandas dataframe
 data = pd.DataFrame(animal_data)
 st.write(data)
 
@@ -43,35 +44,31 @@ feathers = st.sidebar.selectbox("Does the animla have feathers?", ("Yes", "No"))
 lays_eggs = st.sidebar.selectbox("Does the animal lay eggs?", ("Yes", "No"))
 can_fly = st.sidebar.selectbox("Can the animal fly?", ("Yes", "No"))
 
-st.stop()
 
-# # Convert the dictionary to a Pandas dataframe
-# data = pd.DataFrame(animal_data)
 
-# # Encode the categorical target variable as integer labels
-# le = LabelEncoder()
-# y = le.fit_transform(data['Class'])
+# Encode the categorical target variable as integer labels
+le = LabelEncoder()
+y = le.fit_transform(data['Class'])
+X = le.fit_transform(data.drop(columns='Class', 'Animal'])
 
-# # Encode the categorical input features as integer labels using OneHotEncoder
-# ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'), [0])], remainder='passthrough')
-# X = ct.fit_transform(data.drop(columns=['Class','Animal'])
-                     
-# #Data splitting
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+st.write(y)
+st.write(X)
+#Data splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-# # Create a random forest classifier with 100 trees and a maximum depth of 3
-# clf = RandomForestClassifier(n_estimators=100, max_depth=3)
+# Create a random forest classifier with 100 trees and a maximum depth of 3
+clf = RandomForestClassifier(n_estimators=100, max_depth=3)
 
-# # Train the classifier on the training set
-# clf.fit(X_train, y_train)
+# Train the classifier on the training set
+clf.fit(X_train, y_train)
 
-# # Use the trained classifier to predict the class labels of the test set
-# y_pred = clf.predict(X_test)
+# Use the trained classifier to predict the class labels of the test set
+y_pred = clf.predict(X_test)
 
-# # Decode the predicted integer labels back to their original string values
-# y_pred = le.inverse_transform(y_pred)
+# Decode the predicted integer labels back to their original string values
+y_pred = le.inverse_transform(y_pred)
 
-# # Compute the accuracy of the classifier
-# accuracy = accuracy_score(data.loc[y_test.index, 'Class'], y_pred)
+# Compute the accuracy of the classifier
+accuracy = accuracy_score(data.loc[y_test.index, 'Class'], y_pred)
 
-# print("Accuracy:", accuracy)
+print("Accuracy:", accuracy)
