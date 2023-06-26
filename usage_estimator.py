352ce54db7dbfa75ab4b86bdb4cad5cd8da85f05
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-06-26 16:59:55 +0200

    Update usage_estimator.py

--- usage_estimator.py
+++ usage_estimator.py
@@ -9,16 +9,16 @@ with col1:
   st.subheader("Workload: Ingest")
   wh_ingest = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"))
   wh_ingest_hours = st.number_input("How many hours a day will the WH be running?")
-  wh_ingest_days = st.number_input("How many days a week will the WH be running?)
+  wh_ingest_days = st.number_input("How many days a week will the WH be running?")
 
 with col2:
   st.subheader("Workload: Transformations")
   wh_transform = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"))
   wh_transform_hours = st.number_input("How many hours a day will the WH be running?")
-  wh_transform_days = st.number_input("How many days a week will the WH be running?)
+  wh_transform_days = st.number_input("How many days a week will the WH be running?")
 
 with col3:
   st.subheader("Workload: Internal BI")
   wh_internal_ingest = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"))
   wh_internal_hours = st.number_input("How many hours a day will the WH be running?")
-  wh_internal_days = st.number_input("How many days a week will the WH be running?)
+  wh_internal_days = st.number_input("How many days a week will the WH be running?")
