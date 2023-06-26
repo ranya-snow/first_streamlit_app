8c61dc9f16cf8312b8eb57ed86fa1d5ed4490318
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-06-26 16:51:43 +0200

    Update usage_estimator.py

--- usage_estimator.py
+++ usage_estimator.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Welcome to estimating Snowflake consumption")
+
+st.header("EStimate hourly compute and WH size")
+workload_ingest = st.input("Ingest")
+wh_ingest = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"))
