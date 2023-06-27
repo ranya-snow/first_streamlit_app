00bba4cae6fdc01455e6a7e71c116c5452ae4399
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-06-27 16:38:31 +0200

    Update usage_estimator.py

--- usage_estimator.py
+++ usage_estimator.py
@@ -269,23 +269,34 @@ elif cloud == "Google Cloud Platform":
         st.error("Invalid selection")
 
 st.divider()
+#wh credit cost
+xs_price = 1
+s_price = 2
+m_price = 4
+l_price = 8
+xl_price = 16
+2xl_price = 32
+3xl_price = 64
+4xl_price = 128
+5xl_price = 256
+6xl_price = 512
 
 col1, col2, col3 = st.columns(3)
 with col1:
   st.subheader("Workload: Ingest")
-  wh_ingest = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"), key="ingest")
+  wh_ingest = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL"), key="ingest")
   wh_ingest_hours = st.number_input("How many hours a day will the WH be running?", key="ingest_h", step=0.25)
   wh_ingest_days = st.number_input("How many days a week will the WH be running?",  key="ingest_d", step=1)
 
 with col2:
   st.subheader("Workload: Transformations")
-  wh_transform = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"), key="transform")
+  wh_transform = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL"), key="transform")
   wh_transform_hours = st.number_input("How many hours a day will the WH be running?", key="transform_h", step=0.25)
   wh_transform_days = st.number_input("How many days a week will the WH be running?", key="transform_d", step=1)
 
 with col3:
   st.subheader("Workload: Internal BI")
-  wh_internal = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"), key="internal")
+  wh_internal = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL"), key="internal")
   wh_internal_hours = st.number_input("How many hours a day will the WH be running?", key="internal_h", step=0.25)
   wh_internal_days = st.number_input("How many days a week will the WH be running?", key="internal_d", step=1)
 
@@ -293,18 +304,46 @@ st.divider()
 col4, col5, col6 = st.columns(3)
 with col4:
   st.subheader("Workload: Ad-hoc Analysis")
-  wh_adhoc = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"), key="adhoc")
+  wh_adhoc = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL"), key="adhoc")
   wh_adhoc_hours = st.number_input("How many hours a day will the WH be running?", key="adhoc_h", step=0.25)
   wh_adhoc_days = st.number_input("How many days a week will the WH be running?",  key="adhoc_d", step=1)
 
 with col5:
   st.subheader("Workload: Development work")
-  wh_dev = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"), key="dev")
-  wh_dev_hours = st.number_input("How many hours a day will the WH be running?", key="dev_h", step=0.25)
-  wh_dev_days = st.number_input("How many days a week will the WH be running?", key="dev_d", step=1)
+  wh_dev = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL", "6XL"), key="dev")
+  wh_dev_hours = st.number_input("How many hours a day will the WH be running?", key="dev_h", step=0.25, "6XL")
+  wh_dev_days = st.number_input("How many days a week will the WH be running?", key="dev_d", step=1, "6XL")
 
 with col6:
   st.subheader("Workload: Extermal BI")
   wh_external = st.selectbox("Pick a warehouse size", ("XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"), key="external")
   wh_external_hours = st.number_input("How many hours a day will the WH be running?", key="external_h", step=0.25)
   wh_external_days = st.number_input("How many days a week will the WH be running?", key="external_d", step=1)
+
+#ingest credits
+if wh_ingest == "XS":
+    credit_ingest = xs_price
+elif wh_ingest == "S":
+    credit_ingest = s_price
+elif wh_ingest == "M":
+    credit_ingest = m_price
+elif wh_ingest == "L":
+    credit_ingest = l_price
+elif wh_ingest == "XL":
+    credit_ingest = xl_price 
+elif wh_ingest == "2XL":
+    credit_ingest = 2xl_price
+elif wh_ingest = "3XL":
+    credit_ingest = 3xl_price
+elif wh_ingest = "4XL":
+    credit_ingest = 4xl_price
+elif wh_ingest = "5XL":
+    credit_ingest = 5xl_price
+elif wh_ingest = "6XL":
+    credit_ingest = 6xl_price
+
+st.write(credit_ingest)
+st.stop()
+
+credits = wh_ingest 
+st.header(f"Consumption estimation: {credits}")
