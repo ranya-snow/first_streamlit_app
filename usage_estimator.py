b439e6724f0df7b7e36dc43bab84c87be39142db
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-06-29 13:39:46 +0200

    Update usage_estimator.py

--- usage_estimator.py
+++ usage_estimator.py
@@ -484,21 +484,15 @@ st.subheader("Storage")
 st.write(f"Your per TB storage cost is ${storage_price}")
 storage_tb = st.number_input("How many TBs of data will you be storing each month?")
 storage_cost = storage_tb*storage_price*12
-st.metric(label=:orange["orange:[Your annual storage cost]"], value=storage_cost)
+st.metric(label="Your annual storage cost", value=storage_cost)
 
 st.divider()
 
-st.metric(label="Annual credits", value=total_credits)
-st.metric(label="Your total compute cost", value=total_cost_compute)
-total_cost = storage_cost+total_cost_compute
+colx, coly, colz = st.columns(3)
+colx.metric(label="Your annual storage cost", value=storage_cost)
+coly.metric(label="Annual credits", value=total_credits)
+colz.metric(label="Your total compute cost", value=total_cost_compute)
 st.header(f"Your total total cost will be ${total_cost}")
-
-st.stop()
-
-st.write("Your total number of annual credits is:")
-st.write(total_credits)
-st.write(total_cost_compute)
-
 
 credits = wh_ingest 
 st.header(f"Consumption estimation: {credits}")
