71c63a51b99b5c2a95e3b089be7748bb5aa04528
Ranya <111740805+ranya-snow@users.noreply.github.com>
2023-06-29 13:35:23 +0200

    Update usage_estimator.py

--- usage_estimator.py
+++ usage_estimator.py
@@ -484,7 +484,7 @@ st.subheader("Storage")
 st.write(f"Your per TB storage cost is ${storage_price}")
 storage_tb = st.number_input("How many TBs of data will you be storing each month?")
 storage_cost = storage_tb*storage_price*12
-st.metric(label=orange:["orange:[Your annual storage cost]"], value=storage_cost)
+st.metric(label=:orange["orange:[Your annual storage cost]"], value=storage_cost)
 
 st.divider()
 
